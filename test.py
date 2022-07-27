14f284c9686e0ded6953f8c16519419609e2bb4b
hsdjkfnsfc <851897637@qq.com>
2022-07-27 17:30:56 +0800

    uptdate

---
+++ test.py
@@ -0,0 +1,29 @@
+
+import requests
+import os,time,random
+from rdkit import Chem
+from multiprocessing import Pool
+
+def inference(file):
+    api='http://192.168.1.221:1234/infer'
+    file = os.path.abspath(file)
+    # 获取上传文件
+    with requests.Session() as s:
+        result = s.post(api,
+                   files={
+                       'file_upload': open(file, "rb")
+                   },
+                   data={
+                       "molvec": True,
+                       "osra": True,
+                       "imago": True,
+                       "ocmr": True
+                   }).json()
+        return result
+
+
+if __name__ == "__main__":
+    dir = os.path.dirname(os.path.abspath(__file__))
+    file = os.path.join(dir,"test.png")
+    res = inference(file)
+    print(res)
